9ff0aa6b4c86483e338702f139e186d2d7f2b8f8
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2022-05-06 18:45:46 -0300

    Update landing page
    Created a logo component and built the landing page

---
+++ wall4comment/src/Logo.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Logo({ scale }) {
+    return (
+        <div
+            className={`flex text-5xl uppercase text-center justify-center items-center mb-5 scale-[${
+                scale || 1
+            }]`}
+        >
+            <h1 className="absolute -translate-y-3/4 -translate-x-1/2 font-bold w-fit">
+                wall
+            </h1>
+            <h1 className=" text-9xl font-extrabold text-blue-600 lue text-opacity-40">
+                4
+            </h1>
+            <h1 className="absolute translate-y-3/4 translate-x-1/2 font-bold w-fit">
+                comment
+            </h1>
+        </div>
+    );
+}
+
+export default Logo;
