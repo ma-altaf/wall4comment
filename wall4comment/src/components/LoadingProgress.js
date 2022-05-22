8ca97e30d600bcc29bfb3cc39a1c6e8e96d63bad
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2022-05-22 14:06:17 -0300

    Update LoadingProgress.js

--- wall4comment/src/components/LoadingProgress.js
+++ wall4comment/src/components/LoadingProgress.js
@@ -1,7 +1,7 @@
 function LoadingProgress({ progress = 0 }) {
     return (
-        <div className="w-full h-full flex items-center justify-center absolute text-white text-3xl">
-            <span className="backdrop-blur-[1px] bg-black opacity-50 w-full h-full absolute"></span>
+        <div className="w-full h-full flex items-center justify-center absolute text-white text-3xl backdrop-blur-[1px]">
+            <span className="bg-black opacity-50 w-full h-full absolute"></span>
             <p className="z-10">{progress}</p>
         </div>
     );
