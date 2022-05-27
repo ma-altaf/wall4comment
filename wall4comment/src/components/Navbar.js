92502a1b12883161ee50e450b7dcdb9bb97b247a
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2022-05-27 19:12:01 -0300

    Minor visual changes
    - Fixed the x-axis padding on mobile for the navbar and dev mention

--- wall4comment/src/components/Navbar.js
+++ wall4comment/src/components/Navbar.js
@@ -3,7 +3,7 @@ import Logo from "./Logo";
 
 function Navbar() {
     return (
-        <div className="flex justify-between items-center w-full h-16 px-1 md:px-10 z-50">
+        <div className="flex justify-between items-center w-full h-16 px-4 md:px-10 z-50">
             <Logo shrink />
             <Link
                 className="bg-blue-600 w-fit h-fit px-5 py-2 rounded-lg uppercase text-white"
