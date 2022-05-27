c59bece3bf95cb45e5747c6cf4ef6ac5c4895552
ma-altaf <79712014+ma-altaf@users.noreply.github.com>
2022-05-27 18:48:34 -0300

    Visual updates
    - Change Logo component
    - Change Navbar component

--- wall4comment/src/components/Logo.js
+++ wall4comment/src/components/Logo.js
@@ -3,17 +3,31 @@ import React from "react";
 function Logo({ shrink = false }) {
     return (
         <div
-            className={`relative flex text-3xl md:text-5xl uppercase text-center justify-center items-center w-[32rem] h-fit pointer-events-none ${
-                shrink && "scale-50"
-            }`}
+            className={`relative flex uppercase text-center justify-center items-center h-fit pointer-events-none`}
         >
-            <h1 className="absolute -translate-y-3/4 -translate-x-1/2 font-bold w-fit block">
+            <h1
+                className={`font-bold w-fit block ${
+                    shrink
+                        ? "text-xl -translate-y-3 translate-x-4"
+                        : "-translate-y-6 translate-x-8 text-3xl"
+                }`}
+            >
                 wall
             </h1>
-            <h1 className="text-8xl md:text-9xl font-extrabold text-blue-600 lue text-opacity-40">
+            <h1
+                className={`font-extrabold text-blue-600 lue text-opacity-40 ${
+                    shrink ? "text-6xl" : "text-8xl"
+                }`}
+            >
                 4
             </h1>
-            <h1 className="absolute translate-y-3/4 translate-x-1/2 font-bold w-fit">
+            <h1
+                className={`font-bold w-fit ${
+                    shrink
+                        ? "text-xl translate-y-4 -translate-x-3"
+                        : "translate-y-8 -translate-x-6 text-3xl"
+                }`}
+            >
                 comment
             </h1>
         </div>
--- wall4comment/src/components/Navbar.js
+++ wall4comment/src/components/Navbar.js
@@ -3,8 +3,8 @@ import Logo from "./Logo";
 
 function Navbar() {
     return (
-        <div className="flex justify-between items-center w-full h-16 pr-10 z-50">
-            <Logo shrink={true} />
+        <div className="flex justify-between items-center w-full h-16 px-1 md:px-10 z-50">
+            <Logo shrink />
             <Link
                 className="bg-blue-600 w-fit h-fit px-5 py-2 rounded-lg uppercase text-white"
                 to="/signup"
